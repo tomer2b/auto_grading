d1f11ebf7c80d4936288407f66a370aa4be79def
Tomer <auto.install@rabin.com>
2024-08-16 16:36:41 +0300

    a

--- auto_grading/download.py
+++ auto_grading/download.py
@@ -2,18 +2,21 @@ import requests
 
 
 
-
-def download_code(url):
-  response = requests.get(url, headers={'Cache-Control': 'no-cache'})
-  with open(url.split('/')[-1], 'w') as file:
+def download_code(url,target_name):
+  response = requests.get(url, headers={'Cache-Control': 'no-cache'}) # open(url.split('/')[-1], 'w')
+  with open(target_name,'w')  as file:
     file.write(response.content.decode())
 
-  # urllib.request.urlretrieve(url_with_timestamp,url.split('/')[-1])
+
 
 def download_files():
-  url=r'https://raw.githubusercontent.com/RabinSchool/auto_grading/main/auto_gradeing.py'
-  download_code(url)
-  url=r'https://raw.githubusercontent.com/RabinSchool/auto_grading/main/tasks.csv'
-  download_code(url)
+  url=r'https://raw.githubusercontent.com/tomer2b/auto_grading/main/auto_gradeing.py'
+  download_code(url,'auto_gradeing.py')
+  url=r'https://raw.githubusercontent.com/tomer2b/auto_grading/main/tasks.csv'
+  download_code(url,'tasks.csv')
+
+
+
+
 
 download_files()
