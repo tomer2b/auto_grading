2283e917431ffa547012340431cb6f89138b111c
Tomer <auto.install@rabin.com>
2024-08-16 10:18:55 +0300

    add package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="auto_grading",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    description="A module for automatic grading python assignments for students",
+    author="Tomer Tubi",
+    author_email="tomer2b@gmail.com",
+    url="https://github.com/tomer2b/auto_grading",
+    license="GPL-3.0-or-later",  # Specify GPL version
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
