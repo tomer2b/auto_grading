580e4719d829896882b7306e2e786c0ed5a111c3
Tomer <auto.install@rabin.com>
2024-08-16 12:00:21 +0300

    a

--- auto_grading/__init__.py
+++ auto_grading/__init__.py
@@ -1,10 +1,9 @@
 __version__ = '0.1'
 __author__ = 'Tomer Tubi'
 
-# from auto_grading import test_weight,question_weight
-# from auto_grading import questions_dic
-from auto_grading import *
-# from auto_grading import import_tasks
-# from auto_grading import print,input,grade_student_functions,run_test,CheckAssignment
+from auto_gradeing_code import test_weight,question_weight
+from auto_gradeing_code import questions_dic
+from auto_gradeing_code import import_tasks
+from auto_gradeing_code import print,input,grade_student_functions,run_test,CheckAssignment
 
 
