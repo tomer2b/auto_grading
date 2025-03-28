adb2310fa7e91118d968ee5d20af6e62236a99d2
Tomer <auto.install@rabin.com>
2025-03-28 17:20:26 +0300

    save

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auto_grading",
-    version="0.305",
+    version="0.306",
     packages=find_packages(),
     install_requires=[],
     include_package_data=True,
