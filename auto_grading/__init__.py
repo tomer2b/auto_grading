9bbd4f2a3702619583c0824a2afeb5d011f322de
Tomer <auto.install@rabin.com>
2026-07-24 10:33:57 +0300

    save

--- auto_grading/__init__.py
+++ auto_grading/__init__.py
@@ -15,4 +15,5 @@ import pkgutil
 # טעינת הנתונים מהקובץ שנמצא באותה תיקייה
 data = pkgutil.get_data(__name__, 'tasks_db.json')
 tasks_db = json.loads(data.decode('utf-8'))
+api='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'
 
