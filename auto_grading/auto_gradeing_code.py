85c2212af16205c4c57bf506d37f474f12e7cb3b
Tomer <auto.install@rabin.com>
2026-07-26 14:50:32 +0300

    a

--- auto_grading/auto_gradeing_code.py
+++ auto_grading/auto_gradeing_code.py
@@ -314,17 +314,18 @@ class CheckAssignment:
                 ai_help_text=get_student_ai_hint(function_code,tasks_db[str(question_set)][func],expected_result,self.output_lst,return_values,list(result))
 
               
-              return  False,func_call,f'',self.output_lst, list(result),type(ai_help_text)
+              return  False,func_call,f'',self.output_lst, list(result),ai_help_text
               
           
 
         except Exception as e:
+            error_explanation={"NameError":"first error"}
             tb_info = traceback.extract_tb(e.__traceback__)[-1]
             error_type = type(e).__name__
             func_call = func + '(' + str(parms)[1:-1] + ')'
-            ai_help=error_explanations.get(error_type) if error_explanations.get(error_type)!=None else ''
+            ai_help=error_explanation.get(error_type) if error_explanation.get(error_type)!=None else ''
             
-            return False, func_call, f"שגיאת {error_type} בשורה {tb_info.lineno}:\n{tb_info.line}",[],[],'hello tomer sasas'
+            return False, func_call, f"שגיאת {error_type} בשורה {tb_info.lineno}:\n{tb_info.line}",[],[],ai_help
 
 def grade_student_functions(req_functions,student_functions):
     count = 0
