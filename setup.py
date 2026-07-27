571abafcfe36a6f7302723cd4b71cc0e32392c48
Tomer <auto.install@rabin.com>
2026-07-27 09:24:07 +0300

    a

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auto_grading",
-    version="0.3321",
+    version="0.3322",
     packages=find_packages(),
     install_requires=[
         'groq',
