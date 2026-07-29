f21d0fac844f81c332fba19f719b86a5db8cd4e6
Tomer <auto.install@rabin.com>
2026-07-29 13:52:57 +0300

    ש

--- auto_grading/auto_gradeing_code.py
+++ auto_grading/auto_gradeing_code.py
@@ -599,7 +599,7 @@ def generate_terminal_simulation(in_list, expected_out_list, student_out_list):
     # פתיחת חלונית הטרמינל 
     terminal_html = """
     <div dir="ltr" style="background-color: #1e1e1e; color: #d4d4d4; font-family: 'Consolas', 'Courier New', monospace; padding: 15px; border-radius: 6px; border: 1px solid #333; text-align: left; margin-bottom: 15px; font-size: 14px; line-height: 1.2; box-shadow: inset 0 0 10px rgba(0,0,0,0.5);">
-        <div style="color: #569cd6; font-weight: bold; margin-bottom: 8px; border-bottom: 1px solid #333; padding-bottom: 5px;">>_ דוגמת הרצה</div>
+        <div style="color: #569cd6; font-weight: bold; margin-bottom: 8px; border-bottom: 1px solid #333; padding-bottom: 5px;">>_ דוגמת הרצה מבוקשת</div>
     """
     
     # שלב 1: יצירת הקלטים המדומים
